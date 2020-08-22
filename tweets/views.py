d85e56726a774187702732af3301c63e7ffb9dd9
Dexter Cunningham <dextercunningham@hotmail.co.uk>
2020-08-22 11:40:49 +0100

    Add tweet_list_view

--- tweets/views.py
+++ tweets/views.py
@@ -8,8 +8,31 @@ def home_view(request, *args, **kwargs):
     return render(request, "pages/home.html", context={}, status=200)
 
 
-def tweet_detail_view(request, tweet_id, *args, **kwargs):
+def tweet_list_view(request, *args, **kwargs):
+    """REST API VIEW
 
+    Args:
+        request (Request): Standard Request sent to a view.
+
+    Returns:
+        JsonResponse: Data for all Tweets in DB and HTTP Status of request.
+    """
+    qs = Tweet.objects.all()
+    tweets_list = [{"id": x.id, "content": x.content} for x in qs]
+    data = {"response": tweets_list}
+    return JsonResponse(data)
+
+
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    """REST API VIEW
+
+    Args:
+        request (Request): Standard Request sent to a view.
+        tweet_id (int): Id of Tweet to get from DB.
+
+    Returns:
+        JsonResponse: Data for Tweet object and HTTP Status of request.
+    """
     data = {
         "id": tweet_id,
     }
