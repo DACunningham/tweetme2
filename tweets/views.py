1d7965ce6b56f6e83d479dd0e7c9595a714531db
Dexter Cunningham <dextercunningham@hotmail.co.uk>
2020-08-24 15:25:18 +0100

    Update JS Requests and Add Allowed Hosts

--- tweets/views.py
+++ tweets/views.py
@@ -1,15 +1,23 @@
 import random
+from django.conf import settings
 from django.http import Http404, HttpResponse, JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils.http import is_safe_url
+
 from .models import Tweet
 from .forms import TweetForm
+
+ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 
 # Create your views here.
 def tweet_create_view(request, *args, **kwargs):
     form = TweetForm(request.POST or None)
+    next_url = request.POST.get("next") or None
     if form.is_valid():
         obj = form.save(commit=False)
         obj.save()
+        if next_url != None and is_safe_url(next_url, ALLOWED_HOSTS):
+            return redirect(next_url)
         form = TweetForm()
     return render(request, "components/form.html", context={"form": form}, status=200)
 
