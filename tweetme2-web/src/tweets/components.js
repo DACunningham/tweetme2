7a809a5e4f18e648ee7b5e8b4ff783ce8d414ab8
Dexter Cunningham <dextercunningham@hotmail.co.uk>
2020-09-01 12:50:35 +0100

    Refactor Reract Components

--- tweetme2-web/src/tweets/components.js
+++ tweetme2-web/src/tweets/components.js
@@ -2,18 +2,60 @@ import React, { useEffect, useState } from "react";
 
 import { LoadTweets } from "../lookup";
 
+export function TweetsComponent(props) {
+  const textAreaRef = React.createRef();
+  const [newTweets, setNewTweets] = useState([]);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // console.log(event);
+    console.log(textAreaRef.current.value);
+    textAreaRef.current.value = "";
+    const newVal = textAreaRef.current.value;
+    let tempNewTweets = [...newTweets];
+    tempNewTweets.unshift({
+      content: newVal,
+      likes: 0,
+      id: 123123,
+    });
+    setNewTweets(tempNewTweets);
+    // console.log(newVal);
+  };
+
+  return (
+    <div className={props.className}>
+      <div className="col-12">
+        <form onSubmit={handleSubmit}>
+          <textarea
+            ref={textAreaRef}
+            required={true}
+            className="form-control text-dark"
+            name="tweet"
+          ></textarea>
+          <button type="submit" className="btn btn-primary my-3">
+            Tweet
+          </button>
+        </form>
+      </div>
+      <TweetsList newTweets={newTweets} />
+    </div>
+  );
+}
+
 export function TweetsList(props) {
-  const [tweets, setTweets] = useState([]);
+  const [tweetsInit, setTweetsInit] = useState([]);
+  console.log("props.newtweets", props.newTweets);
   useEffect(() => {
     const myCallback = (response, status) => {
       console.log(response, status);
       if (status === 200) {
-        setTweets(response);
+        setTweetsInit(response);
+      } else {
+        alert("Error");
       }
     };
     LoadTweets(myCallback);
   }, []);
-  return tweets.map((item, index) => {
+  return tweetsInit.map((item, index) => {
     return (
       <Tweet
         tweet={item}
@@ -26,18 +68,31 @@ export function TweetsList(props) {
 
 export function ActionBtn(props) {
   const { tweet, action } = props;
+  const [likes, setLikes] = useState(tweet.likes ? tweet.likes : 0);
+  const [userLike, setUserLike] = useState(
+    tweet.userLike === true ? true : false
+  );
   const className = props.className
     ? props.className
     : "btn btn-primary btn-sm";
   const actionDisplay = action.display ? action.display : "Action";
-  const display =
-    action.type === "like" ? `${tweet.likes} ${actionDisplay}` : actionDisplay;
+
   const handleClick = (event) => {
     event.preventDefault();
     if (action.type === "like") {
-      console.log(tweet.likes + 1);
+      if (userLike === true) {
+        setLikes(likes - 1);
+        setUserLike(false);
+      } else {
+        setLikes(likes + 1);
+        setUserLike(true);
+      }
     }
   };
+
+  const display =
+    action.type === "like" ? `${tweet.likes} ${actionDisplay}` : actionDisplay;
+
   return (
     <button className={className} onClick={handleClick}>
       {display}
